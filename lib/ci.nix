96f93f404a8c867ebe9ae505ae922323cec02bb3
Jansen <n.jansen@devsaur.de>
2022-07-14 21:05:21 +0200

    feat: add a combined buildJobs output

--- lib/ci.nix
+++ lib/ci.nix
@@ -2,36 +2,51 @@
 with builtins; with lib; {
 
   generateBuildJobs = flake: system:
-    flattenAttrs [
+    rec {
+      combined = flake.legacyPackages.${system}.runCommand "build-jobs-${system}" { } ''
+        mkdir -p $out && cd $out
 
-      #nixosConfigurations
-      (if flake ? nixosConfigurations then
-        mapAttrs'
-          (name: metaConfig:
-            nameValuePair'
-              "nixos-system-${name}"
-              metaConfig.config.system.build.toplevel
-          )
-          (filterAttrs
-            (name: metaConfig: metaConfig.system == system)
-            flake.nixosConfigurations
-          )
-      else { })
+        ${concatStringsSep "\n" (
+          mapAttrsToList
+            (name: drv: "ln -s ${drv} ${name}")
+            jobs
+        )}
+      '';
 
-      # packages
-      (if flake ? packages then
-        mapAttrs'
-          (n: v: nameValuePair' "pkg-${n}" v)
-          flake.packages.${system}
-      else { })
 
-      # devShells
-      (if flake ? devShells then
-        mapAttrs'
-          (n: v: nameValuePair' "shell-${n}" v)
-          flake.devShells.${system}
-      else { })
+      jobs = flattenAttrs [
 
-    ];
+        #nixosConfigurations
+        (if flake ? nixosConfigurations then
+          mapAttrs'
+            (name: metaConfig:
+              nameValuePair'
+                "nixos-system-${name}"
+                metaConfig.config.system.build.toplevel
+            )
+            (filterAttrs
+              (name: metaConfig: metaConfig.system == system)
+              flake.nixosConfigurations
+            )
+        else { })
+
+        # packages
+        (if flake ? packages then
+          mapAttrs'
+            (n: v: nameValuePair' "pkg-${n}" v)
+            flake.packages.${system}
+        else { })
+
+        # devShells
+        (if flake ? devShells then
+          mapAttrs'
+            (n: v: nameValuePair' "shell-${n}"
+              v)
+            flake.devShells.${system}
+        else { })
+
+      ];
+    };
 
 }
+
