31f034f719c6058cec8e71536bc20faaea0ce7d3
Philipp Hochkamp <git@phochkamp.de>
2022-08-18 17:09:42 +0200

    feat: generateDarwinConfiguration

--- lib/configurations.nix
+++ lib/configurations.nix
@@ -1,6 +1,36 @@
 { lib, ... }:
 with builtins; with lib; {
 
+  generateDarwinConfigurations = inputs:
+    mapAttrs
+      (name: _:
+        let
+          flake = inputs.self;
+          metaConfig = import "${flake}/darwin-hosts/${name}" { inherit (flake) inputs lib; };
+        in
+        metaConfig // (inputs.darwin.lib.darwinSystem {
+          inherit (metaConfig) system;
+          inputs = {
+            inherit inputs flake;
+            pkgs = loadPkgs inputs { inherit (metaConfig) system; };
+            lib = flake.lib;
+            system = metaConfig.system;
+          };
+          modules = flatten [
+
+            # Set the host name from the directory name
+            (mkHostNameModule name)
+
+            # Add the modules from all inputs
+            (inputDarwinModules inputs)
+
+            # the system configuration
+            metaConfig.modules
+          ];
+        })
+      )
+      (readDir "${inputs.self}/darwin-hosts");
+
   generateNixosConfigurations = inputs:
     mapAttrs
       (name: _:
@@ -22,7 +52,7 @@ with builtins; with lib; {
             (mkHostNameModule name)
 
             # Add the modules from all inputs
-            (inputModules inputs)
+            (inputNixOSModules inputs)
 
             # the system configuration
             metaConfig.modules
@@ -33,11 +63,36 @@ with builtins; with lib; {
 
   mkHostNameModule = name:
     { config, ... }: {
-      system.name = mkDefault name;
-      networking.hostName = config.system.name;
+      networking.hostName = mkDefault name;
     };
 
-  inputModules = inputs:
+  inputDarwinModules = inputs:
+    (filter
+      (module: (typeOf module) == "lambda")
+      (flatten
+        (mapAttrsToList
+          (name: input:
+            if input ? nixosModules
+            then
+              (
+                let
+                  modules = input.darwinModules;
+                in
+                if (typeOf modules) == "set"
+                then attrValues modules
+                else modules
+              )
+            else
+              if input ? darwinModule
+              then input.darwinModule
+              else [ ]
+          )
+          inputs
+        )
+      )
+    );
+
+  inputNixOSModules = inputs:
     (filter
       (module: (typeOf module) == "lambda")
       (flatten
