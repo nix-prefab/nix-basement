947ce7636d53e3f4eddb037af6aa374b699cf857
nzbr <mail@nzbr.de>
2022-09-27 21:08:29 +0200

    feat: lib.mkEnableableModule

--- lib/modules.nix
+++ lib/modules.nix
@@ -19,4 +19,18 @@ with builtins; with lib; {
       )
       (find ".nix" modulesPath);
 
+  # Takes a path to an option, a description of a module and that module and wraps the module, so that it may be enabled by setting the newly created option to true
+  mkEnableableModule = optionPath: description: module: (
+    let
+      evaluated = module { inherit config lib pkgs modulesPath; };
+    in
+    { ... }: {
+      options = recursiveUpdate
+        evaluated.options
+        (setAttrByPath optionPath (mkEnableOption description));
+
+      config = mkIf (getAttrFromPath optionPath config) evaluated.config;
+    }
+  );
+
 }
