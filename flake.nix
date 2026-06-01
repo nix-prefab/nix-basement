26f3fdd281ab76e89ae8e9b40cfc96943863a9fa
Lukas Mertens <git@lukas-mertens.de>
2026-06-01 10:16:06 +0200

    chore: update to nixos 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "TODO: add description";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nmd = {
       url = "github:nix-basement/nmd";
       flake = false;
